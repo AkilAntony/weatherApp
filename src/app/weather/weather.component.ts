9374909c5351bd3e11273df77ea0f7f5a42bdeab
unknown <akhilal778@gmail.com>
2023-09-19 16:47:05 +0530

    second Commit

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -6,44 +6,44 @@ import { GetWeatherDeatailsService } from '../get-weather-deatails.service';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  defaultPlace:string='Nagercoil';
-  date:any;
-  checkInput!:boolean;
-  country!:string;
-  temperature!:number;
-  seasons!:string;
-  day:any
-  weatherDetails:any;
-  private weekday= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
- 
-  constructor(private weatherDetailsservice:GetWeatherDeatailsService) { }
+  defaultPlace: string = 'Nagercoil';
+  date: any;
+  checkInput!: boolean;
+  country!: string;
+  temperature!: number;
+  seasons!: string;
+  day: any
+  weatherDetails: any;
+  private weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  constructor(private weatherDetailsservice: GetWeatherDeatailsService) { }
 
   ngOnInit(): void {
     this.getDateAndDay()
-    this.weatherDetailsservice.getWeatherDetails(this.defaultPlace).subscribe((details=>{
-      this.weatherDetails=details;
-      this.temperature=Math.round(this.weatherDetails.main.temp);
-      this.seasons=this.weatherDetails.weather[0].main;
+    this.weatherDetailsservice.getWeatherDetails(this.defaultPlace).subscribe((details => {
+      this.weatherDetails = details;
+      this.temperature = Math.round(this.weatherDetails.main.temp);
+      this.seasons = this.weatherDetails.weather[0].main;
     }));
   }
 
-  getDateAndDay(){
-    this.date=new Date();
-    this.day=this.weekday[this.date.getDay()];
+  getDateAndDay() {
+    this.date = new Date();
+    this.day = this.weekday[this.date.getDay()];
   }
 
- getUserInputs(input:any){
-    if(input!==""){
-     this.weatherDetailsservice.getWeatherDetails(input).subscribe((details=>{
-     this.weatherDetails=details;
-     this.seasons=this.weatherDetails.weather[0].main;
-     this.temperature=Math.round(this.weatherDetails.main.temp);
-     this.country=this.weatherDetails.sys.country
-     this.checkInput=false;
-    }));
+  getUserInputs(input: any) {
+    if (input !== "") {
+      this.weatherDetailsservice.getWeatherDetails(input).subscribe((details => {
+        this.weatherDetails = details;
+        this.seasons = this.weatherDetails.weather[0].main;
+        this.temperature = this.weatherDetails.main.temp;
+        this.country = this.weatherDetails.sys.country
+        this.checkInput = false;
+      }));
     }
-    else{
-     this.checkInput=true;
+    else {
+      this.checkInput = true;
     }
   }
 }
